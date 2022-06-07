639090c0ae42008951ea11f46f8781bf9e965575
Luiz Romário Santana Rios <luizromario@tecgraf.puc-rio.br>
2022-06-07 19:16:06 -0300

    Add support to eos serialization

--- main.cpp
+++ main.cpp
@@ -4,17 +4,90 @@
 #include "people_repo.h"
 
 #include <fstream>
+#include <functional>
 #include <iostream>
+#include <variant>
 
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/archive/text_oarchive.hpp>
+#include <eos/portable_iarchive.hpp>
+#include <eos/portable_oarchive.hpp>
 
-const char* cachefile_name = "older_than_cache.txt";
+const char* boost_cachefile_name = "older_than_cache.txt";
+const char* eos_cachefile_name = "older_than_cache.eos.bin";
+
+struct boost_archive_option {};
+struct eos_archive_option {};
+using archive_option = std::variant<boost_archive_option, eos_archive_option>;
+
+template <typename Action>
+void with_iarchive(boost_archive_option, Action action)
+{
+    std::ifstream cache{boost_cachefile_name};
+    if (!cache) return;
+
+    boost::archive::text_iarchive ar{cache};
+    action(ar);
+}
+
+template <typename Action>
+void with_iarchive(eos_archive_option, Action action)
+{
+    std::ifstream cache{eos_cachefile_name, std::ios::binary};
+    if (!cache) return;
+
+    eos::portable_iarchive ar{cache};
+    action(ar);
+}
+
+template <typename Action>
+void with_iarchive(archive_option opt, Action action)
+{
+    std::visit([&](auto&& o){ with_iarchive(o, action); }, opt);
+}
+
+template <typename Action>
+void with_oarchive(boost_archive_option, Action action)
+{
+    std::ofstream cache{boost_cachefile_name};
+    boost::archive::text_oarchive ar{cache};
+    action(ar);
+}
+
+template <typename Action>
+void with_oarchive(eos_archive_option, Action action)
+{
+    std::ofstream cache{eos_cachefile_name, std::ios::binary};
+    eos::portable_oarchive ar{cache};
+    action(ar);
+}
+
+template <typename Action>
+void with_oarchive(archive_option opt, Action action)
+{
+    std::visit([&](auto&& o){ with_oarchive(o, action); }, opt);
+}
 
 int main(int argc, char* argv[])
 {
-    if (argc != 3) {
-        std::cerr << "    Usage: " << argv[0] << " <person1> <person2>\n";
+    if (argc != 4) {
+        std::cerr
+            << "    Usage: " << argv[0] << " <archive-type> <person1> <person2>\n"
+            << "      archive-type: \"text\", \"eos\"\n";
+        return -1;
+    }
+
+    std::string optString = argv[1];
+    std::string person1Name = argv[2];
+    std::string person2Name = argv[3];
+
+    archive_option opt;
+    if (optString == "text")
+        opt = boost_archive_option{};
+    else if (optString == "eos")
+        opt = eos_archive_option{};
+    else {
+        std::cerr << "Invalid archive-type: " << optString << "\n";
         return -1;
     }
 
@@ -33,19 +106,13 @@ int main(int argc, char* argv[])
     }
 
     older_than older;
+    with_iarchive(opt, [&](auto& ar)
     {
-        std::ifstream cache{cachefile_name};
-        if (cache) {
-            boost::archive::text_iarchive ar{cache};
-            auto& h = ar.template get_helper<older_than_load_helper>();
-            h.repo = &repo;
+        auto& h = ar.template get_helper<older_than_load_helper>();
+        h.repo = &repo;
 
-            ar >> older;
-        }
-    }
-
-    std::string person1Name = argv[1];
-    std::string person2Name = argv[2];
+        ar >> older;
+    });
 
     auto person1 = repo.find_person(person1Name);
     if (!person1) {
@@ -64,10 +131,10 @@ int main(int argc, char* argv[])
     else
         std::cout << person1->name << " is not older than " << person2->name << "\n";
 
-    std::ofstream cache{cachefile_name};
-
-    boost::archive::text_oarchive ar{cache};
-    ar << older;
+    with_oarchive(opt, [&](auto& ar)
+    {
+        ar << older;
+    });
 
     return 0;
 }
